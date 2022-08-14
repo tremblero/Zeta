d99ddedfe0b0ec27adcf4e83247dbe93fd5c82f3
Trembling <103013062+tremblero@users.noreply.github.com>
2022-08-14 19:30:11 -0400

    2

--- get.js
+++ get.js
@@ -1,11 +1,13 @@
 let s = document.body.getElementsByClassName('version_id')
 
 if (s[0]){
-s[0].innerHTML = 1
+s[0].innerHTML = 2
 } else {
 let newEl = document.createElement("p")
-newEl.innerHTML = 1
+newEl.innerHTML = 2
 newEl.className = "version_id"
 newEl.style = "display:none"
 document.body.append(newEl)
 }
+
+
